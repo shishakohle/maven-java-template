5b61ebc4956df281b3536a45c723bc28feb72441
iw <iw@DESKTOP-RG97KC1.ammonit.local>
2020-02-05 14:12:40 +0100

    Set up a template for a Maven Java project with log4j and JUnit 5.

---
+++ src/test/java/at/institution/projectname/Test.java
@@ -0,0 +1,13 @@
+package at.institution.projectname;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class Test 
+{
+    @Test
+    public void test()
+    {
+        assertTrue(true);
+    }
+}
